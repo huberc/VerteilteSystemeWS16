57659266b636c7a2c87f457770791e46fa7e67d9
Yannick <y.schwarenthorer@icloud.com>
2017-01-12 16:25:30 +0100

    implement secure channel

--- dslab16/src/main/java/channel/SecureChannel.java
+++ dslab16/src/main/java/channel/SecureChannel.java
@@ -1,25 +1,58 @@
 package channel;
 
-import java.io.IOException;
+import security.AuthenticationException;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.IvParameterSpec;
 
 public class SecureChannel extends ChannelDecorator implements  Runnable{
 
-	public SecureChannel(Channel decoratedChannel){
+	private SecretKey key;
+	private Cipher cipherDecrypt, cipherEncrypt;
 
-		//TODO make auth
+	public SecureChannel(Channel decoratedChannel,SecretKey key) throws AuthenticationException{
 
 		super(decoratedChannel);
+
+		this.key = key;
+
+		try {
+			cipherDecrypt = Cipher.getInstance("AES/CTR/NoPadding");
+			cipherEncrypt = Cipher.getInstance("AES/CTR/NoPadding");
+		}catch (Exception e){
+			throw new AuthenticationException(e.getMessage());
+		}
 	}
 	
 	@Override
-	public void write(String response) throws IOException{
-		
-		decoratedChannel.write(response+"***secured***");
+	public void write(String response) throws Exception{
+
+		byte[] responseBytes = response.getBytes();
+
+		//Init cipher
+		IvParameterSpec ivSpec = new IvParameterSpec(responseBytes);
+		cipherEncrypt.init(Cipher.ENCRYPT_MODE, key, ivSpec);
+
+		//Encrypt
+		byte[] encryptedMessage = cipherEncrypt.doFinal(responseBytes);
+
+		decoratedChannel.write(new String(encryptedMessage));
 	}
 	
 	@Override
-	public String read() throws IOException{
-		return this.decoratedChannel.read();
+	public String read() throws Exception{
+
+		byte[] data = this.decoratedChannel.read().getBytes();
+
+		//Init cipher
+		IvParameterSpec ivSpec = new IvParameterSpec(data);
+		cipherDecrypt.init(Cipher.DECRYPT_MODE, key, ivSpec);
+
+		//Decrypt
+		byte[] decryptedMessage = cipherEncrypt.doFinal(data);
+
+		return new String(decryptedMessage);
 	}
 
 	@Override
